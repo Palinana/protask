e1e01fe32f5d9e701c41879e5b01d751cd303e31
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2019-12-06 20:31:43 -0500

    Added dropdown menu to the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,7 +11,26 @@
             </v-toolbar-title>
 
             <v-spacer></v-spacer>
-            
+
+            <!-- Dropdown Menu -->
+            <v-menu offset-y>
+                <template v-slot:activator="{ on }">
+                    <v-btn color="grey" text v-on="on">
+                        <v-icon class="mr-2" style="font-size: 15px">fas fa-chevron-down</v-icon>
+                        <span>Menu</span>
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item v-for="link in links" :key="link.text" @click="method()" link>
+                        <router-link :to="link.route" style="text-decoration: none">                        
+                            <v-list-item-content>
+                                <v-list-item-title class="font-weight-light">{{ link.text }}</v-list-item-title>
+                            </v-list-item-content>
+                        </router-link>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+             
             <!-- Nav menu -->
             <div class="my-2">
                 <v-btn text class="grey--text">
@@ -24,29 +43,23 @@
         
         <!-- Side drawer -->
         <v-navigation-drawer v-model="drawer" absolute temporary class="primary">
-            <v-list class="primary rounded">
-                <v-list-tile 
-                    v-for="link in links" 
-                    :key="link.text"
+            <v-list dense nav>
+                <v-list-item
+                v-for="link in links" 
+                :key="link.text"
+                link
                 >
-                <v-hover v-slot:default="{ hover }">
-                    <v-list-item :class="`${hover && 'grey'}`">
-                        <router-link :to="link.route">
-                            <v-list-item-icon>
-                                <v-icon class="white--text" style="font-size: 20px">fas fa-{{ link.icon }}</v-icon>
-                            </v-list-item-icon>
-
-                        
-                            <v-list-tile-content>
-                                <v-list-tile-title class="white--text font-weight-light">{{ link.text }}</v-list-tile-title>
-                            </v-list-tile-content>
-                        </router-link>
-                    </v-list-item>
-                    </v-hover>
-                </v-list-tile>
+                    <v-list-item-icon>
+                        <v-icon class="white--text" style="font-size: 20px">fas fa-{{ link.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <router-link :to="link.route">
+                        <v-list-item-content>
+                            <v-list-item-title class="white--text font-weight-light">{{ link.text }}</v-list-item-title>
+                        </v-list-item-content>
+                    </router-link>
+                </v-list-item>
             </v-list>
         </v-navigation-drawer>
-
     </nav>
 </template>
 
